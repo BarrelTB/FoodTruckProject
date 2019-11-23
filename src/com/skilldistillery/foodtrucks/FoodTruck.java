c35c4748c26fbde8a2a3c988abc6f8a1c3957206
Caleb Whitford <calebwhitford@Administrators-MacBook-Pro-2.local>
2019-11-22 17:54:34 -0700

    Added fields to FoodTruck Object

---
+++ src/com/skilldistillery/foodtrucks/FoodTruck.java
@@ -0,0 +1,17 @@
+package com.skilldistillery.foodtrucks;
+
+public class FoodTruck {
+	
+	// F I E L D S
+	private int iD;
+	private String name;
+	private String foodType;
+	private int rating;
+	
+	// C O N S T R U C T O R S
+	
+	// M E T H O D S
+	
+
+
+}
