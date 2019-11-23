88a8d4d9a404c4baa18e64ba48a5587ffe1237e8
Caleb Whitford <calebwhitford@Administrators-MacBook-Pro-2.local>
2019-11-22 17:59:41 -0700

    Added constructors to FoodTruck, created FoodTruckApp object, created FoodTruck array

--- src/com/skilldistillery/foodtrucks/FoodTruck.java
+++ src/com/skilldistillery/foodtrucks/FoodTruck.java
@@ -8,7 +8,15 @@ public class FoodTruck {
 	private String foodType;
 	private int rating;
 	
+	
 	// C O N S T R U C T O R S
+	public FoodTruck(int iD, String name, String foodType) {
+		super();
+		this.iD = iD;
+		this.name = name;
+		this.foodType = foodType;
+	}
+	
 	
 	// M E T H O D S
 	
--- src/com/skilldistillery/foodtrucks/FoodTruckApp.java
+++ src/com/skilldistillery/foodtrucks/FoodTruckApp.java
@@ -4,7 +4,17 @@ public class FoodTruckApp {
 
 	public static void main(String[] args) {
 		
+		FoodTruckApp app = new FoodTruckApp();
+		
+		FoodTruck[] trucks = new FoodTruck[5];
+		
+	
+		
+		//You will create a separate class with a main method that starts the program.
+		// It will have an array to store up to five FoodTruck objects. 
+		// The main method of this class is the only static method in the entire project.
 
 	}
+	public void run();
 
 }
